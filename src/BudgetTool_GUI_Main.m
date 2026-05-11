8ee66545a877d2474b40fb193e5b06ad0980508a
rtighe <rytighe@gmail.com>
2026-05-11 16:43:09 -0400

    Draft push - main GUI is defined. Need to further define button callbacks.

--- src/BudgetTool_GUI_Main.m
+++ src/BudgetTool_GUI_Main.m
@@ -14,7 +14,7 @@ function handles = BudgetTool_GUI_Main()
 
     fig = uifigure( ...
         'Name',     'Budget_Utility', ...
-        'Position', [100 100 420 254], ...
+        'Position', [100 100 420 340], ...
         'Resize',   'off', ...
         'Color',    [0.15 0.15 0.15] ...
     );
@@ -23,9 +23,9 @@ function handles = BudgetTool_GUI_Main()
     % over handles can reference handles.fig immediately. 
 
     handles.fig = fig;
-    handles.budgetFiles = {};
+    handles.transactionFiles = {};
     handles.outputDir = '';
-    handles.analysisResults = [];
+    handles.analyzeTransactions = [];
 
     % ---------------------------------------------------------------------
     % Layout Constants
@@ -37,5 +37,139 @@ function handles = BudgetTool_GUI_Main()
     FIELD_H =       36;     % Height of Output Directory field
     GAP =           12;     % Gap between buttons
     TOP_MARGIN =    20;     % Spacing from top of figure
+    FIG_H =         340;    % Pixel height of overall figure
+
+    % Building top to bottom - set constants for current y
+
+    curY = FIG_H - TOP_MARGIN;
+
+    % Helper function to align next set of congrols down by a control
+    % height followed by a constant gap (GAP = 12 set above). 
+
+    function y = nextY(controlH)
+        
+        curY = curY - controlH - GAP;
+        y = curY;
+
+    end
+
+    % ---------------------------------------------------------------------
+    % Section Label Helper Function
+    % Sets consistent labels with input text at position y. 
+    % ------------------------------------------------------------------
+    function makeLabel(txt, y)
+        uilabel(fig, ...
+            'Text',             txt, ...
+            'Position',         [LEFT, y, WIDTH, 18], ...
+            'FontSize',         10, ...
+            'FontColor',        [0.65 0.65 0.65], ...
+            'FontWeight',       'bold', ... 
+            'BackgroundColor',  [0.15 0.15 0.15] ... 
+        );
+    end
+
+    % ---------------------------------------------------------------------
+    % 1. OUTPUT DIRECTORY
+    % Un-Editable display field + user select button side-by-side top GUI
+    % ---------------------------------------------------------------------
+
+    makeLabel('Output Directory',nextY(18));
+
+    fieldW =    240;
+    btnW2 =     130;  
+    spacing =   10;
+
+    handles.outputDirField = uitextarea(fig, ...
+        'Position',         [LEFT, nextY(FIELD_H), fieldW, FIELD_H], ...
+        'Value',            'No Output Directory Selected', ...
+        'Editable',         'off', ...
+        'FontSize',         11, ...
+        'FontColor',        [0.85 0.85 0.85], ...
+        'BackgroundColor',  [0.22 0.22 0.22] ...
+    );
+
+    % Set output directory button next to text area field. 
+
+    fieldY = curY;  %Same row
+
+    handles.btnOutputDir = uibutton(fig, ...
+        'Text',             'Specify Output Directory', ...
+        'Position',         [LEFT + fieldW + spacing, fieldY, btnW2, FIELD_H], ...
+        'FontSize',         10, ...
+        'FontWeight',       'bold', ...
+        'BackgroundColor',  [0.20 0.45 0.75], ...
+        'FontColor',        [1 1 1], ...
+        'ButtonPushedFcn',  @(btn,evt) cb_specifyOutputDir(handles) ...
+     );
+
+    % ---------------------------------------------------------------------
+    % Divider
+    % ---------------------------------------------------------------------
+
+    makeLabel('________________________________', nextY(14) - 4);
+
+    % ---------------------------------------------------------------------
+    % 2. BUTTON TO LOAD TRANSACTION FILE
+    % ---------------------------------------------------------------------
+    
+    makeLabel('Load Transaction File', nextY(14) - 4);
+
+    handles.btnLoadTransactions = uibutton(fig, ...
+        'Text',             'Load Transaction File', ...
+        'Position',         [LEFT, nextY(BUTTON_H), WIDTH, BUTTON_H], ...
+        'FontSize',         13, ...
+        'FontWeight',       'bold', ...
+        'BackgroundColor',  [0.18 0.52 0.35], ...
+        'FontColor',        [ 1 1 1], ...
+        'ButtonPushedFcn',  @(btn,evt) cb_loadTransactionFile(handles) ...
+    );
+
+    % ---------------------------------------------------------------------
+    % Divider Label
+    % ---------------------------------------------------------------------
+
+    makeLabel('______________________________', nextY(14) - 4);
+
+    % ---------------------------------------------------------------------
+    % 3. RUN ANALYSIS
+    % ---------------------------------------------------------------------
+
+    makeLabel('Analyze Transactions', nextY(14) + 4);
+
+    handles.btnRunAnalysis = uibutton(fig, ...
+        'Text',             'Analyze Transactions', ...
+        'Position',         [LEFT, nextY(BUTTON_H), WIDTH, BUTTON_H], ...
+        'FontSize',         13, ...
+        'FontWeight',       'bold', ...
+        'BackgroundColor',  [0.18 0.52 0.35], ...
+        'FontColor',        [1 1 1], ...
+        'ButtonPushedFcn',  @(btn,evt) cb_analyzeTransactions(handles) ...
+    );
+
+
+end % Budget_Tool_GUI_Main
+
+% =========================================================================
+% CALLBACKS for Button Events
+% =========================================================================
+            
+function cb_specifyOutputDir(handles)
+    % TODO - Define Callback
+    disp('[cb_specifyOutputDir] not yet defined.')
+end
+
+function cb_loadTransactionFile(handles)
+    % TODO - Define Callback
+    disp('[cb_loadTransactionFile] not yet defined.')
+end
+
+function cb_analyzeTransactions(handles)
+    % TODO - Define Callback
+    disp('[cb_analyzeTransactions not yet defined.')
+end
+
+
+
+
 
 
